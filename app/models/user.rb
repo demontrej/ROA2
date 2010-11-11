28c94f35b4c001f6e7236c07a026455b15276808
demontrej <juanc.com@gmail.com>
2010-11-11 14:25:36 -0400

    casa 11/11(usuarios)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :username, :email, :password, :password_confirmation, :role, :status
+  attr_accessible :username, :email, :password, :password_confirmation, :role, :status, :sexo, :nombreapellido, :pais, :ciudad, :idioma, :fechanacimiento,:cargo, :skype
   acts_as_authentic
   ROLES = %w[admin teacher student]
+  SEXO = %w[_ hombre mujer]
+  ESTADO= %w[activo inactivo]
   #para autenticarse el usernme o email
   def self.find_by_anything(login)
     find_by_username(login) || find_by_email(login)
